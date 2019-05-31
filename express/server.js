76e095e934073e0c77b5d7940dbe754e4280f6e2
AnkitGupta1990 <ankitgupta.mangal@gmail.com>
2019-05-31 18:11:51 +0530

    Update server.js

--- express/server.js
+++ express/server.js
@@ -6,6 +6,11 @@ const bodyParser = require('body-parser');
 
 const router = express.Router();
 router.get('/', (req, res) => {
+   console.log("start refresh");
+setInterval(function() {
+  console.log("I am doing my 5 minutes check");
+  // do your stuff here
+}, 1000);
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<h1>Hello from Express.js!</h1>');
   res.end();
